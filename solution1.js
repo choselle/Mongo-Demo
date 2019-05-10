fdcc25ee954ac701796c256284a4186327ca5596
Chase Hoselle <choselle@gmail.com>
2019-05-09 21:17:34 -0500

    cleaned up solution1.js

--- solution1.js
+++ solution1.js
@@ -1,44 +1,22 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/mongo-exercises")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("Could not connect to MongoDB..."));
+mongoose.connect("mongodb://localhost/mongo-exercises");
 
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: Date, default: Date.now },
+  date: Date,
   isPublished: Boolean,
   price: Number
 });
 
+const Course = mongoose.model("Course", courseSchema);
+
 async function getCourses() {
-  const Course = mongoose.model("Course", courseSchema);
-  //const courses = await Course.find({ isPublished: true, tags: "backend" })
-  // .sort({ name: 1 })
-  // .select({ name: 1, author: 1 });
-  const courses = await Course.find();
-  return courses;
-}
-
-async function updateCourse(id) {
-  // Approach: Query first
-  const course = await Course.findById(id);
-  if (!course) return;
-
-  course.isPublished = true;
-  course.auther - "Another Author";
-
-  const result = await course.save();
-  console.log(result);
-}
-
-async function removecourse(id) {
-  const result = await Course.deleteOne({ _id: id });
-
-  console.log(result);
+  return await Course.find({ isPublished: true, tags: "backend" })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
 }
 
 async function run() {
@@ -46,4 +24,4 @@ async function run() {
   console.log(courses);
 }
 
-getCourses();
+run();
